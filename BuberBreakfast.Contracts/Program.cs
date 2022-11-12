000bf37c95d35c1ea73f515a5b526d23f6abc4de
MarcoGarthi97 <marcogarthi@gmail.com>
2022-11-12 01:50:08 +0100

    Aggiungere i file di progetto.

---
+++ BuberBreakfast.Contracts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuberBreakfast.Contracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
