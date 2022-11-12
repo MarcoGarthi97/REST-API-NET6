f06b67f215974212187ebfe665ce0db132a0ef30
MarcoGarthi97 <marcogarthi@gmail.com>
2022-11-12 02:25:27 +0100

    Add services for API

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -1,7 +1,10 @@
 
+using BuberBreakfast.Services.Breakfasts;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    builder.Services.AddScoped<IBreakfastService, BreakfastService>();
 }
 
 var app = builder.Build();
